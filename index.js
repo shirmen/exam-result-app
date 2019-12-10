847e76d922178ca912addeb0a4d9993f10022c54
Myagmarsuren Shirmenbaatar <migaakuz@gmail.com>
2019-12-11 00:29:57 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'exam'
+});
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get('/', function(request, response) {
+    if (request.session.logged_in) {
+        response.render('result', {data: request.session.data});
+    } else {
+        response.render('index', {province: "", grade: "", code: "", message: ""});
+    }
+});
+
+app.post('/', function(request, response) {
+    console.log(request.body);
+    var province = request.body.province;
+    var grade = request.body.grade;
+    var code = request.body.code;
+
+    if (province && grade && code) {
+        connection.query('SELECT * FROM results2019 WHERE province = ? AND grade = ? AND id = ?', [province, grade, code], function(error, results, fields) {
+            if (results.length > 0) {
+                request.session.data = results[0];
+                request.session.logged_in = true;
+                response.redirect('/');
+            } else {
+                response.render('index', {
+                    province: province,
+                    grade: grade,
+                    code: code,
+                    message: "Таны оруулсан мэдээлэл алдаатай байна. Дахин шалгана уу.",
+                });
+            }
+            response.end();
+        });
+    }
+});
+
+app.get('/logout', function(request, response) {
+    request.session.destroy((err) => {
+        if (err) {
+            return console.log(err);
+        }
+        response.redirect('/');
+    });
+});
+
+app.listen(3000);
